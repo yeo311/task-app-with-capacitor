3a109c1cb91bcaa2b9163b696fb7e1914fbd4339
Ji Hoon Yeo <jihoonyeo@Yeo-MacBook-Air.local>
2024-04-27 23:21:16 +0900

    setup capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'task-app-with-capacitor',
+  webDir: 'dist'
+};
+
+export default config;
